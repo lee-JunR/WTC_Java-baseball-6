e5546a7fe88be3dc499761e208fef378119cbd06
lee-JunR <wns2610@icloud.com>
2023-11-25 23:00:42 +0900

    chore : "변수명 변경"

--- src/main/java/baseball/domain/BallNumber.java
+++ src/main/java/baseball/domain/BallNumber.java
@@ -4,7 +4,7 @@ public class BallNumber {
 
   private final static int MAX_BASEBALL_NUMBER = 9;
   private final static int MIN_BASEBALL_NUMBER = 1;
-  private final static String OUT_OF_RANGE = "숫자는 1~9 사이여야합니다.";
+  private final static String OUT_OF_RANGE_ERROR_MESSAGE = "숫자는 1~9 사이여야합니다.";
   private int ballNumber;
 
 
@@ -19,7 +19,7 @@ public class BallNumber {
 
   private void validate(int number) {
     if (MIN_BASEBALL_NUMBER > number || MAX_BASEBALL_NUMBER < number) {
-      throw new IllegalArgumentException(OUT_OF_RANGE);
+      throw new IllegalArgumentException(OUT_OF_RANGE_ERROR_MESSAGE);
     }
   }
 }
