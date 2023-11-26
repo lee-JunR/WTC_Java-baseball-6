58b579f9fb1136d65ca4a4d47e82cb9b8887b14e
lee-JunR <wns2610@icloud.com>
2023-11-27 01:21:25 +0900

    refactor : "불변객체의 final 키워드 추가"

--- src/main/java/baseball/domain/BallNumber.java
+++ src/main/java/baseball/domain/BallNumber.java
@@ -5,7 +5,7 @@ public class BallNumber {
   private final static int MAX_BASEBALL_NUMBER = 9;
   private final static int MIN_BASEBALL_NUMBER = 1;
   private final static String OUT_OF_RANGE_ERROR_MESSAGE = "숫자는 1~9 사이여야합니다.";
-  private int ballNumber;
+  private final int ballNumber;
 
 
   public BallNumber(int number) {
